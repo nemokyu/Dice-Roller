ee9661945e675074d4d12f1f3c75509e1c7f1dff
deon <84358266+nemokyu@users.noreply.github.com>
2023-07-03 07:48:52 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+// random image generation 1
+var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSource);
+
+// random image generation 2
+var randomNumber2 = Math.floor(Math.random() * 6) + 1; //1-6
+
+var randomDiceImage2 = "dice" + randomNumber2 + ".png"; //dice1.png - dice6.png
+
+var randomImageSource2 = "images/" + randomDiceImage2; //images/dice1.png - images/dice6.png
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomImageSource2);
+
+// changing the title 
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩"
+} else {
+    document.querySelector("h1").innerHTML = "Draw !"
+}
+
+
+
+
